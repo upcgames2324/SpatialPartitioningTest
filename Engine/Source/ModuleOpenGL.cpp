b1bc952902a1f826e043b77d4d76715067a66901
Bermellet <jnietomaldonado@gmail.com>
2023-10-23 11:42:01 +0200

    First class changes

--- Engine/Source/ModuleOpenGL.cpp
+++ Engine/Source/ModuleOpenGL.cpp
@@ -18,6 +18,21 @@ bool ModuleOpenGL::Init()
 {
 	LOG("Creating Renderer context");
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY); // or CORE
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+
+	// Window
+	ModuleWindow* window = new ModuleWindow;
+	SDL_GL_CreateContext(window->window);
+
+	SDL_WINDOW_OPENGL(ModuleWindow);
+	SDL_GL_CreateContext(window->window);
+
 	return true;
 }
 
